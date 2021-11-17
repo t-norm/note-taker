1ffcd855d12b10a2695dcf9d67fb8e0dc84735c5
Tyler Norman <tnorman600@gmail.com>
2021-11-17 11:55:29 -0700

    added fs and path dependencies, middleware for the public folder and JSON, and routes for index.html and notes.html

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const fs = require("fs");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
